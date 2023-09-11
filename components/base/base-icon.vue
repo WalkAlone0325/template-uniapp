8c991efa8e885c73c6274fd5fc2cfb295e5bf35d
Nebula <jsw0325@163.com>
2023-09-11 09:28:32 +0800

    feat

---
+++ components/base/base-icon.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  type: String,
+  color: String,
+  custom: {
+    type: Boolean,
+    default: false
+  },
+  customPrefix: {
+    type: String,
+    default: 'iconfont'
+  },
+  size: {
+    type: Number,
+    default: 24
+  }
+})
+defineEmits(['click'])
+</script>
+
+<template>
+  <uni-icons :type="type" :custom-prefix="custom && customPrefix" :size="size" :color="color" @click="$emit('click')" />
+</template>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,71 @@
 <template>
-  <view class="container">
-    <view class="intro">
-      本项目已包含uni
-      ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。
+  <view class="page">
+    <page-head :title="title"></page-head>
+
+    <view
+      class="uni-mx-10 uni-my-10"
+      style="height: 40rpx; border: 1px solid #000; background-color: aquamarine"
+    ></view>
+    <view style="height: 40rpx; border: 1px solid #000; margin: 40rpx; background-color: blue"></view>
+
+    <uni-icons type="icon-daohang" custom-prefix="iconfont"></uni-icons>
+    <uni-icons type="icon-dengji" custom-prefix="iconfont" size="30"></uni-icons>
+    <uni-icons type="icon-daka" custom-prefix="iconfont" size="30"></uni-icons>
+
+    <base-icon type="icon-daohang" custom @click="clickIcon"></base-icon>
+    <base-icon type="spinner-cycle"></base-icon>
+
+    <view class="uni-loadmore">
+      <uni-load-more status="more"></uni-load-more>
     </view>
-    <text class="intro">详见：</text>
-    <uni-link :href="href" :text="href"></uni-link>
+
+    <uni-list>
+      <uni-list-item title="列表文字" note="列表描述信息"></uni-list-item>
+      <uni-list-item :disabled="true" title="列表文字" note="列表禁用状态"></uni-list-item>
+    </uni-list>
+
+    <view class="uni-card">
+      <uni-collapse>
+        <uni-collapse-item title="默认开启" :open="true">
+          <text>折叠内容</text>
+        </uni-collapse-item>
+        <uni-collapse-item title="折叠内容">
+          <text>折叠内容</text>
+        </uni-collapse-item>
+        <uni-collapse-item title="禁用状态" disabled>
+          <text>折叠内容</text>
+        </uni-collapse-item>
+      </uni-collapse>
+
+      <view class="uni-list">
+        <view
+          class="uni-list-cell uni-collapse"
+          v-for="(list, index) in lists"
+          :key="index"
+          :class="index === lists.length - 1 ? 'uni-list-cell-last' : ''"
+        >
+          <view
+            class="uni-list-cell-navigate uni-navigate-bottom"
+            hover-class="uni-list-cell-hover"
+            :class="list.show ? 'uni-active' : ''"
+            @click="trigerCollapse(index)"
+          >
+            {{ list.title }}
+          </view>
+          <view class="uni-list uni-collapse" :class="list.show ? 'uni-active' : ''">
+            <view
+              class="uni-list-cell"
+              hover-class="uni-list-cell-hover"
+              v-for="(item, key) in list.item"
+              :key="key"
+              :class="key === list.item.length - 1 ? 'uni-list-cell-last' : ''"
+            >
+              <view class="uni-list-cell-navigate uni-navigate-right">{{ item }}</view>
+            </view>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -13,18 +73,41 @@
 export default {
   data() {
     return {
-      href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+      title: 'list-with-collapses',
+      lists: [
+        {
+          title: '产品',
+          show: false,
+          item: ['iOS', 'Android', 'HTML5']
+        },
+        {
+          title: '方案',
+          show: false,
+          item: ['PC方案', '手机方案', 'TV方案']
+        },
+        {
+          title: '新闻',
+          show: false,
+          item: ['公司新闻', '行业新闻']
+        }
+      ]
     }
   },
-  onLoad() {},
-  methods: {}
+  methods: {
+    trigerCollapse(e) {
+      for (let i = 0, len = this.lists.length; i < len; ++i) {
+        if (e === i) {
+          this.lists[i].show = !this.lists[i].show
+        } else {
+          this.lists[i].show = false
+        }
+      }
+    },
+    clickIcon() {
+      console.log(uni.$u, '111')
+    }
+  }
 }
 </script>
 
-<style>
-.container {
-  padding: 20px;
-  font-size: 14px;
-  line-height: 24px;
-}
-</style>
+<style></style>
